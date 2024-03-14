0a1cacb099c70f9cb34e9d1642d97625410246f7
ARPHAM <121754526+ARPHAM@users.noreply.github.com>
2024-03-14 08:32:37 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
+        String fileIn = "input.txt";
+        String fileOut = "output.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileIn));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
 
+            String replacedWord = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
 
-        Bài làm viết tại đây
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut));
+            writer.write(replacedWord);
+            writer.close();
 
+            System.out.println("OK");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
